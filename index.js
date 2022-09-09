1dc3aefb4894c14bffc72e628890ea4015ce09a8
Piero <piero.pilco.r@uni.pe>
2022-09-09 13:09:05 -0500

    Patch added to index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
+const jobOffer = require("./models/jobOffer");
 
 const mongoDBenv = process.env.DATABASE_URL;
 const port = process.env.PORT || 5000;
@@ -21,6 +22,7 @@ const app = express();
 
 app.use(cors())
 app.use(express.json());
+app.use(express.text());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
@@ -33,6 +35,22 @@ app.get("/", function (req, res) {
 });
 
 app.use('/', require('./routes/jobs'))
+
+app.patch("/jobs/:id/:attrtochange", async (req, res) => {
+  const {id, attrtochange} = req.params;
+  try {
+    const updateJob = await jobOffer.findByIdAndUpdate(
+      id,
+      { [attrtochange]: req.body },
+      {
+        new: true,
+      }
+    );
+    res.status(200).json(updateJob);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 
 app.listen(port, () => console.log(`Perfect Port ${port}`));
